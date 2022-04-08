21c4126af9f75154857dbd768bbcc7ea6efa3bff
codenyxcode <harsh1739371@gmail.com>
2022-04-08 09:00:01 +0530

    added web-devicons in plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,6 +65,7 @@ return packer.startup(function(use)
     }
     use "p00f/nvim-ts-rainbow"
     use "nvim-treesitter/playground"
+    use "kyazdani42/nvim-web-devicons"
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
